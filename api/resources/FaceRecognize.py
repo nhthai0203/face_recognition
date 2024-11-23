312442bedc2fdea477ef6ae8ec082adaabfce7f5
LamLam <nguyenbaolamss030205@gmail.com>
2024-11-23 11:03:46 +0700

    blank

--- api/resources/FaceRecognize.py
+++ api/resources/FaceRecognize.py
@@ -1,19 +1,7 @@
 import face_recognition
+from flask_restful import Resource
+from flask import request
 
 class FaceRecognize(Resource):
-    def post(self):
-        # Get the image from the request
-        image = request.files['image']
-
-        # Save the image to a temporary file
-        image_path = '/tmp/' + image.filename
-        image.save(image_path)
-
-        # Load the image
-        image = cv2.imread(image_path)
-
-        # Detect faces in the image
-        face_locations = face_recognition.face_locations(image)
-
-        # Return the face locations
-        return {'face_locations': face_locations}
+    def get(self):
+        
